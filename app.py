21f49d957fa11c6e409e580e9493cd00a7fd336a
Stella Frank <stella.frank@gmail.com>
2023-02-02 17:30:22 +0100

    Fresh start for working repo.

---
+++ app.py
@@ -0,0 +1,67 @@
+import random
+
+# from uuid import uuid4 # for user ids
+import itertools
+
+consequent_integers = itertools.count()  # for user ids
+
+from flask import (
+    Flask,
+    render_template,
+    redirect,
+    url_for,
+    request,
+    make_response,
+    session,
+)
+from file_management import get_images_labelling_list, write_new_row
+
+
+app = Flask(__name__)
+app.secret_key = b"--a]a1k?qOd#=/VLc2*I0M6[y"
+
+image_list = list()
+bird_count = 0
+
+
+@app.route("/")
+def index():
+    session["number"] = consequent_integers.__next__()
+    return render_template("home.html")
+
+
+@app.route("/birds", methods=["POST"])
+def save_birds():
+    response = make_response(redirect(url_for("birds")))
+    # Saves labels to CSV file
+    response_dict = dict(request.form.items())  # request the POST-ed info
+    # print(labelledDict)
+    write_new_row(
+        response_dict, session_id=session["number"]
+    )  # make sure this format works
+    global bird_count
+    bird_count += 1
+    return response
+
+
+@app.route("/birds")
+def birds():
+    global image_list  # pairs of bird files
+    if len(image_list) == 0:
+        image_list = get_images_labelling_list()  # default length=100
+    else:
+        print(f"Images Left: {len(image_list) - bird_count}")
+    try:
+        f1, f2 = image_list[bird_count]  # image pairs to rate
+        if random.random() > 0.5:
+            f1, f2 = f2, f1
+        return render_template(
+            "birds.html",
+            image1=f1,
+            image2=f2,
+        )  # can add more options here if necessary
+    except IndexError:  # no more bird pairs
+        return render_template("finished.html")
+
+
+app.run(debug=True, port=3000, host="0.0.0.0")
---
+++ file_management.py
@@ -0,0 +1,78 @@
+import csv
+import datetime
+import glob
+import os
+from pathlib import Path
+import random
+import time
+
+# Change file paths to your local paths
+PAIRS_CSV = "pairs.csv"
+
+BIRDIMAGES = "static/Images/"
+
+# Writes a new row of data into csv file
+# data is: (bird_img1, bird_img2, same?=T/F)
+def write_new_row(data, session_id):
+    fieldnames = ["session_id", "image1", "image2", "same?"]  # , "different"]
+    file_exists = os.path.isfile(PAIRS_CSV)
+    with open(PAIRS_CSV, "a", newline="") as csvfile:
+        dataWriter = csv.DictWriter(
+            csvfile,
+            fieldnames=fieldnames,
+            restval=0,
+            lineterminator="\n",
+        )
+        if not file_exists:
+            dataWriter.writeheader()  # file doesn't exist yet, write a header
+        # 'same?' is a boolean column
+        row_data = {}
+        row_data["session_id"] = session_id
+        # either 'same' or 'different' will be in data, depending on buttonpress
+        row_data["same?"] = "same" in data.keys()
+        if not row_data["same?"]:
+            print("row_data", row_data)
+            print("data", data)
+            assert data["different"] == "Different"
+        row_data["image1"] = Path(data["image1"]).name
+        row_data["image2"] = Path(data["image2"]).name
+        dataWriter.writerow(row_data)
+
+
+# Checks if a pair of images already exists in CSV file
+# (regardless of order)
+def is_pair_labelled(filename1, filename2, CSVPATH):
+    with open(CSVPATH, newline="") as csvfile:
+        my_content = csv.reader(csvfile, delimiter=",")
+        is_labelled = False
+
+        for row in my_content:
+            if (filename1 == row[0] and filename2 == row[1]) or (
+                filename2 == row[0] and filename1 == row[1]
+            ):
+                is_labelled = True
+                return True
+        if not is_labelled:
+            return False
+
+
+# Gets list of image pairs to be labelled (random pairing)
+def get_images_labelling_list(N=100):
+    """
+    Return N pairs of images, chosen at random from static/birds/*jpgs.
+    """
+    pairs_to_label = set()
+    csv_exists = os.path.isfile(PAIRS_CSV)
+    images_glob = set(glob.glob(BIRDIMAGES + "/*.jpg"))
+    print("images", len(images_glob))
+    # If CSV File Exists
+    for i in range(N):
+        (f1, f2) = random.sample(images_glob, 2)
+        if f2 < f1:  # unique pairs by enforcing ordering
+            f1, f2 = f2, f1
+        if csv_exists:
+            if not is_pair_labelled(f1, f2, PAIRS_CSV):
+                pairs_to_label.add((f1, f2))
+        else:
+            pairs_to_label.add((f1, f2))
+    return list(pairs_to_label)
